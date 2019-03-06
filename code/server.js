8e01e3a36507e302177651fa841397f15850859d
Markus Lautenbach <markus.lautenbach@contentful.com>
2019-03-06 22:54:51 +0100

    Use async persistence

--- code/server.js
+++ code/server.js
@@ -1,3 +1,5 @@
+// noinspection ES6UnusedImports
+import regeneratorRuntime from 'regenerator-runtime'
 import React from 'react'
 import express from 'express'
 import { createServer } from 'http'
@@ -8,47 +10,49 @@ import render from './src/server/render'
 import { create as createStore } from './src/server/store'
 import requestToLocation from './src/server/requestToLocation'
 import lowdb from 'lowdb'
-import FileSync from 'lowdb/adapters/FileSync'
+import FileAsync from 'lowdb/adapters/FileAsync'
 
-const db = lowdb(new FileSync('data/db.json'))
-const port = process.env.PORT || 3000
-const app = express()
-const http = createServer(app)
-const io = listen(http)
+(async function () {
+  const db = await lowdb(new FileAsync('data/db.json'))
+  const port = process.env.PORT || 3000
+  const app = express()
+  const http = createServer(app)
+  const io = listen(http)
 
-app.use('/static', express.static(__dirname + '/dist'))
+  app.use('/static', express.static(__dirname + '/dist'))
 
-let store
+  let store
 
-function startPersisting() {
-  db.set('redux', store.getState()).write()
-  setTimeout(startPersisting, 2000)
-}
+  async function startPersisting() {
+    await db.set('redux', store.getState()).write()
+    setTimeout(startPersisting, 2000)
+  }
 
-app.get('*', function (request, response) {
-  fs.readFile(
-    path.resolve(__dirname, 'dist/index.html'),
-    (_, template) => {
-      if (!store) {
-        store = createStore(db.get('redux').value())
-        startPersisting()
-      }
-      store.dispatch({
-        type: 'USER_NAVIGATION',
-        payload: { newLocation: requestToLocation(request) },
-      })
-      response.send(render(template.toString(), store))
-    },
-  )
-})
-
-io.on('connection', socket => {
-  console.log('a user connected')
-  socket.on('actions', (actions, callback) => {
-    actions.forEach(store.dispatch)
-    callback(true)
+  app.get('*', async function (request, response) {
+    fs.readFile(
+      path.resolve(__dirname, 'dist/index.html'),
+      async (_, template) => {
+        if (!store) {
+          store = createStore(await db.get('redux').value())
+          startPersisting()
+        }
+        store.dispatch({
+          type: 'USER_NAVIGATION',
+          payload: { newLocation: requestToLocation(request) },
+        })
+        response.send(render(template.toString(), store))
+      },
+    )
   })
-})
 
-http.listen(port)
-console.log('server started on port ' + port)
+  io.on('connection', socket => {
+    console.log('a user connected')
+    socket.on('actions', (actions, callback) => {
+      actions.forEach(store.dispatch)
+      callback(true)
+    })
+  })
+
+  http.listen(port)
+  console.log('server started on port ' + port)
+})()
