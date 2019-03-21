9f09518f6f19f7bbbf358343a2cf3fb741decd41
Markus Lautenbach <markus.lautenbach@contentful.com>
2019-03-21 15:11:23 +0100

    Split ui to capture and clarify

--- code/src/shared/components/pages/Capture.js
+++ code/src/shared/components/pages/Capture.js
@@ -1,6 +1,28 @@
 import React from 'react'
+import uuidv4 from 'uuid/v4'
+import { connect } from 'react-redux'
 
-export default () =>
+export default connect(
+  null,
+  dispatch => ({
+    onConfirm: (title) => dispatch({ type: 'CREATE_TASK', payload: { newTask: { title, id: uuidv4() } } }),
+  }),
+)(({ onConfirm }) =>
   <div>
     <h1>Capture</h1>
-  </div>
+    <input
+      type="text"
+      autoFocus
+      onKeyDown={(e) => {
+        if (e.key === 'Enter' && e.target.value.length > 0) {
+          onConfirm(e.target.value)
+          e.target.value = ''
+        }
+      }}
+      css={{
+        fontFamily: '\'Quicksand\', sans-serif',
+        marginBottom: '.5rem',
+      }}
+    />
+  </div>,
+)
