077fea1d1b9b99dd4eca833666e03255e4304ab0
Markus Lautenbach <markus.lautenbach@contentful.com>
2019-03-24 01:39:48 +0100

    Enable adding to home screen
    Requirement for share target

--- code/src/client/index.js
+++ code/src/client/index.js
@@ -4,4 +4,14 @@ import { Provider } from 'react-redux'
 import Main from '../shared/components/Main'
 import store from './store'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/static/assets/sw.js').then(function(registration) {
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 hydrate(<Provider store={store}><Main /></Provider>, document.getElementById('app'))
